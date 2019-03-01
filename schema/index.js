8f27a96a6a006eb4b3f867791e41fa42432ca074
bmartel <brandonmartel@gmail.com>
2019-02-28 21:06:16 -0600

    resolve page schema properly

--- schema/index.js
+++ schema/index.js
@@ -3,13 +3,13 @@ import user from "./user";
 import task from "./task";
 import comment from "./comment";
 
-export const page = new schema.Entity("page", {
+export const page = {
   user,
   task,
   tasks: [task],
   comment,
   comments: [comment]
-});
+};
 
 export default state => {
   return normalize(state, page);
